1cbe77b26053d2995bde266814bb4cdb761a716e
Codnpix <pierrejarriges@gmail.com>
2018-11-18 16:28:58 +0100

    limitation du mouvement de la camera aux limites de la map

--- js/main.js
+++ js/main.js
@@ -5,16 +5,19 @@ function preload() {
     mapImage = loadImage('img/map.png');
 }
 function setup() {
-    createCanvas(400, 300);
+    
+    let canvas = createCanvas(400, 300);
     myMap = createSprite(400, 300, 800, 600);
     myMap.addImage('map', mapImage);
-    mySprite = createSprite(200, 100, 20, 20);
+    mySprite = createSprite(canvas.width/2, canvas.height/2, 20, 20);
     mySprite.shapeColor = color(250, 0, 0, 255);
+    /*tourner le sprite pour qu'il soit tourné vers le bas*/
     mySprite.rotation = -90;
 }
 function draw() {
     background(220);
     drawSprite(myMap);
+    /*faire bouger le sprite (mamie) avec les fleches*/
       if(keyDown(LEFT_ARROW))
       {
           mySprite.position.x -= 1;
@@ -32,10 +35,25 @@ function draw() {
         {
             mySprite.position.y += 1;
         }
+    /*la caméra suit le sprite*/
+    camera.position.x = mySprite.position.x;
+    camera.position.y = mySprite.position.y;
     
+    /*limiter le mouvement de la caméra au bord de la map*/
+    if (camera.position.x < canvas.width /2) {
+        camera.position.x = canvas.width / 2;
+    }
+    if (camera.position.x > (myMap.width - (canvas.width / 2))) {
+        camera.position.x = myMap.width - (canvas.width / 2);
+    }
+    if (camera.position.y < canvas.height / 2 ) {
+        camera.position.y = canvas.height / 2;
+    }
+    if (camera.position.y > (myMap.height - (canvas.height / 2))) {
+        camera.position.y = myMap.height - (canvas.height / 2);
+    }
     
-      camera.position.x = mySprite.position.x;
-      camera.position.y = mySprite.position.y;
+      
     
       if(mySprite.position.x < 0)
         mySprite.position.x = 0;
