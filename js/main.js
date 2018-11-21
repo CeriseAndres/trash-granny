977241071e4a5319963b3aedeb2d7aa7d5abe850
CeriseAndres <cerise.andres@yahoo.fr>
2018-11-21 10:11:46 +0100

    spawnCats OK

--- js/main.js
+++ js/main.js
@@ -6,6 +6,12 @@ let myCat1;
 let myCat2;
 let myCat3;
 
+    let cat1spawn = false;
+    let cat2spawn = false;
+    let cat3spawn = false;
+
+
+
 function preload() {
     mapImage = loadImage('img/map.png');
 
@@ -38,11 +44,13 @@ function setup() {
     myCat1 = createSprite(500,300,20,20);
     myCat1.addAnimation('normal', "img/sprites_cat/cat1_walk1.png", "img/sprites_cat/cat1_walk2.png", "img/sprites_cat/cat1_walk3.png");
     
-    myCat2 = createSprite(100,200,20,20);
+    myCat2 = createSprite(50,10,20,20);
     myCat2.addAnimation('normal', "img/sprites_cat/cat2_walk1.png", "img/sprites_cat/cat2_walk2.png", "img/sprites_cat/cat2_walk3.png");
 
-    myCat3 = createSprite(50,10,20,20);
+    myCat3 = createSprite(800,500,20,20);
     myCat3.addAnimation('normal', "img/sprites_cat/cat3_walk1.png", "img/sprites_cat/cat3_walk2.png", "img/sprites_cat/cat3_walk3.png");
+
+
 }
 function draw() {
     background(220);
@@ -51,17 +59,17 @@ function draw() {
     drawSprite(myMap);
     /*faire bouger le sprite (mamie) avec les fleches*/
       if(keyDown(LEFT_ARROW)){
-          mySprite.position.x -= 4;
+          mySprite.position.x -= 1;
       }
       if(keyDown(RIGHT_ARROW)){
-          mySprite.position.x += 4;
+          mySprite.position.x += 1;
       }
       if(keyDown(UP_ARROW)){
-        mySprite.position.y -= 4 ;
+        mySprite.position.y -= 1;
         }
 
-        if(keyDown(DOWN_ARROW)) {
-            mySprite.position.y += 4;
+      if(keyDown(DOWN_ARROW)) {
+            mySprite.position.y += 1;
         }
     /*la caméra suit le sprite*/
     camera.position.x = mySprite.position.x;
@@ -94,25 +102,54 @@ function draw() {
     
       drawSprite(mySprite);
       drawSprites(maisons);
-    /*function spawnCats (){
-        if (mySprite.position.y = 190) {
+
+    /*Apparition des chats à une ordonnée précise de la mamie*/
+    function drawCat(cat) {
+        drawSprite(cat);
+    }
+
+   function spawnCat() {
+        if ((mySprite.position.x == 200)) {
+            drawCat(myCat1);
+            cat1spawn = true;
             
-            myCat1.maxSpeed = 3;
-            myCat1.attractionPoint(0.08, mySprite.position.x, mySprite.position.y);
+        }
+        if (mySprite.position.x == 300) {
+            drawCat(myCat2);
+            cat2spawn = true;
+        }
+        if (mySprite.position.x == 500) {
+            drawCat(myCat3);
+            cat3spawn = true;
+        }
+        
+    }
+ 
+    function updateCats() {
+        if (cat1spawn) {
+            myCat1.maxSpeed = 2;
+            myCat1.attractionPoint(0.05, mySprite.position.x, mySprite.position.y);
             drawSprite(myCat1);
         }
-    }*/
-    myCat1.maxSpeed = 3;
-    myCat1.attractionPoint(0.08, mySprite.position.x, mySprite.position.y);
-    drawSprite(myCat1);
 
-    myCat2.maxSpeed = 3;
-    myCat2.attractionPoint(0.08, mySprite.position.x, mySprite.position.y);
-    drawSprite(myCat2);
+        if (cat2spawn) {
+            myCat2.maxSpeed = 2;
+            myCat2.attractionPoint(0.05, mySprite.position.x, mySprite.position.y);
+            drawSprite(myCat2);
+        }
 
-    myCat3.maxSpeed = 3;
-    myCat3.attractionPoint(0.08, mySprite.position.x, mySprite.position.y);
-    drawSprite(myCat3);
+        if (cat3spawn) {
+        myCat3.maxSpeed = 2;
+        myCat3.attractionPoint(0.05, mySprite.position.x, mySprite.position.y);
+        drawSprite(myCat3);
+        }
 
-      camera.off();
+    }
+
+    spawnCat();
+    updateCats();
+
+
+    camera.off();
 }
+
