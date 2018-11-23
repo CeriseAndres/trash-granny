dc6cdb3f8ddfe3a247d26105b63be3ac475f04f5
Codnpix <pierrejarriges@gmail.com>
2018-11-23 19:21:10 +0100

    mise en place des maisons

--- js/main.js
+++ js/main.js
@@ -1,59 +1,120 @@
-let myMap;
+let myMap; //declaration de la map et de son image
 let mapImage;
-let mySprite;
-let maisons;
+
+
+let mami;//decl. mami et son image
+let mamImage;
+
+//decl. maisons
+let house1;
+let house2;
+let house3;
+let house4;
+let house5;
+let house6;
+let house7;
+let house8;
+let houseTrash; //local poubelle
+
+let houses;  //groupe de toutes les maisons pour traitement de groupe
+
+//decl. images maison
+let houseImage1;
+let houseImage2;
+let houseImage3;
+let houseImage4;
+let houseImage1rev;
+let trashImage;
+
 
 function preload() {
+    //fond de la map
     mapImage = loadImage('img/map.png');
+    //image de la mamie
+    mamImage = loadImage('img/mami.png');
+    //images des maisons
+    houseImage1 = loadImage('img/maison1.png');
+    houseImage2 = loadImage('img/maison2.png');
+    houseImage3 = loadImage('img/maison3.png');
+    houseImage4 = loadImage('img/maison4.png');
+    houseImage1rev = loadImage('img/maison1rev.png');
+    trashImage = loadImage('img/local-poubelle.png');
 }
 
 function setup() {
+    //initialisation de la map :
     let canvas = createCanvas(800, 600);
-    myMap = createSprite(800, 600, 1600, 1200);
+    myMap = createSprite(800, 600);
     myMap.addImage('map', mapImage);
+    houses = new Group();
+    //initialisation des maisons :
+    house1 = createSprite(190,120);
+    house1.addImage('maison1', houseImage1);
+    houses.add(house1);
     
-    maisons = new Group();
-    let maison1 = createSprite(80, 80, 100, 100);
-    maison1.shapeColor = color(0,0,255);
-    maisons.add(maison1);
-    let maison2 = createSprite(400, 150, 100, 100);
-    maison2.shapeColor = color(0,0,255);
-    maisons.add(maison2);
-    let maison3 = createSprite(80, 350, 100, 100);
-    maison3.shapeColor = color(0,0,255);
-    maisons.add(maison3);
-    let maison4 = createSprite(600, 300, 100, 100);
-    maison4.shapeColor = color(0,0,255);
-    maisons.add(maison4);
+    house2 = createSprite(850, 60);
+    house2.addImage('maison3', houseImage3);
+    houses.add(house2);
     
-    mySprite = createSprite(120, 185, 20, 20);
-    mySprite.shapeColor = color(250, 0, 0, 255);
+    house3 = createSprite(1400, 70);
+    house3.addImage('maison2', houseImage2);
+    houses.add(house3);
+    
+    house4 = createSprite(160, 600);
+    house4.addImage('maison3', houseImage3);
+    houses.add(house4);
+    
+    house5 = createSprite(795, 350);
+    house5.addImage('maison4', houseImage4);
+    houses.add(house5);
+    
+    house6 = createSprite(1450, 515);
+    house6.addImage('maison1rev', houseImage1rev);
+    houses.add(house6);
+    
+    house7 = createSprite(170, 1115);
+    house7.addImage('maison2', houseImage2);
+    houses.add(house7);
+    
+    house8 = createSprite(780, 1000);
+    house8.addImage('maison1rev', houseImage1rev);
+    houses.add(house8);
+    
+    houseTrash = createSprite(1415, 1020);
+    houseTrash.addImage('localPoub', trashImage);
+    houses.add(houseTrash);
+    
+
+    mami = createSprite(1300, 750);
+    mami.addImage('stand', mamImage);
+    
     /*tourner le sprite pour qu'il soit tourné vers le bas*/
-    mySprite.rotation = -90;
+    mami.rotation = -90;
+    
+    
     
 }
 function draw() {
     background(220);
-    mySprite.collide(maisons);
     
     drawSprite(myMap);
     /*faire bouger le sprite (mamie) avec les fleches*/
       if(keyDown(LEFT_ARROW)){
-          mySprite.position.x -= 4;
+          mami.position.x -= 4;
       }
       if(keyDown(RIGHT_ARROW)){
-          mySprite.position.x += 4;
+          mami.position.x += 4;
       }
       if(keyDown(UP_ARROW)){
-        mySprite.position.y -= 4 ;
+        mami.position.y -= 4 ;
         }
 
         if(keyDown(DOWN_ARROW)) {
-            mySprite.position.y += 4;
+            mami.position.y += 4;
         }
     /*la caméra suit le sprite*/
-    camera.position.x = mySprite.position.x;
-    camera.position.y = mySprite.position.y;
+    camera.position.x = mami.position.x;
+    camera.position.y = mami.position.y;
     
     /*limiter le mouvement de la caméra au bord de la map*/
     if (camera.position.x < canvas.width /2) {
@@ -70,17 +131,23 @@ function draw() {
     }
     
     /*limiter le mouvement du sprite aux limites de la map. (calcul un peu bizarre car les coordonnées de la classe sprite sont données pour son centre)*/
-    if((mySprite.position.x - (mySprite.width / 2)) < 0)
-        mySprite.position.x = 0 + mySprite.width / 2;
-    if((mySprite.position.x + (mySprite.width / 2)) > myMap.width)
-        mySprite.position.x = myMap.width - mySprite.width / 2;
-    if((mySprite.position.y - (mySprite.height / 2)) < 0)
-        mySprite.position.y = 0 + mySprite.height / 2;
-
-    if((mySprite.position.y + (mySprite.height / 2)) > myMap.height)
-        mySprite.position.y = myMap.height - mySprite.height / 2;
+    if((mami.position.x - (mami.width / 2)) < 0)
+        mami.position.x = 0 + mami.width / 2;
+    if((mami.position.x + (mami.width / 2)) > myMap.width)
+        mami.position.x = myMap.width - mami.width / 2;
+    if((mami.position.y - (mami.height / 2)) < 0)
+        mami.position.y = 0 + mami.height / 2;
+    if((mami.position.y + (mami.height / 2)) > myMap.height)
+        mami.position.y = myMap.height - mami.height / 2;
     
-      drawSprite(mySprite);
-    drawSprites(maisons);
-      camera.off();
+    //collisions : 
+    mami.collide(houses);
+    
+    //drawSprite(maison1);
+    drawSprite(mami);
+    //drawSprite(autre);
+    drawSprites(houses);
+    
+    camera.off();
+    
 }
